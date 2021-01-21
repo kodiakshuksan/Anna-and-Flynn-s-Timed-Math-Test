0ce72fc54612fad09c0913537e4398557f3a9c86
kodiakshuksan <74173891+kodiakshuksan@users.noreply.github.com>
2021-01-20 23:41:59 -0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Anna-and-Flynn-s-Timed-Math-Test
+# Code Quiz
+by Jennifer Detmering
+
+## Description
+
+## Deployment
+
+## Picture of project
+
+## Technologies
+
+
+## Challenges
+1/18/2021-Designed project on paper using starting with an outline of the possible javascript. Focusing first on functionality
+1/19/2021-Created the HTML, CSS, Javascript, and README files. 
+1/20/2021-Developing code quiz outline further by using comments. Started writing the code for this project.
+
+## Contributors 
---
+++ script.js
@@ -0,0 +1,33 @@
+//JavaScript Code Quiz Game//
+
+//Click start button to start game//
+"start" button
+handler
+event Listener
+
+//Timer starts//
+Timer starts when button is clicked 
+if start button is clicked then timer starts
+
+//Question #1 appears//
+Question alert box
+alert/prompt
+
+//When I answer question next question appears//
+button submits answer and returns new question
+confirm and submit button
+alert/prompt with new question
+
+//When I answer Question incorrectly the time is subtracted from the clock//
+if question is answered incorrectly then the time is subtracted from the Timer
+
+//when all questions are answered or the timer reaches 0 the game is over//
+if all questions are answered or if the timer reaches 0 the game is over
+
+//When game is over I can save my initials and my score//
+when game is over I can save my initials and my score
+fill in initials and submit button
+event listener/handler
+
+//game resets and starts over//
+if button is clicked then game resets and returns to starting screen
